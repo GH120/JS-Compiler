8862da6e3b5d9959a73e2519bea04eb3398210a6
GH120 <felipevieiraduarte195@gmail.com>
2025-01-28 11:03:03 -0300

    Parsing preditivo demo funcional

--- index.js
+++ index.js
@@ -1,51 +1,96 @@
 import { Lexer } from "./src/lexer.js";
-import { Parser } from "./src/parser.js";
+import { Parser, PredictiveParser } from "./src/parser.js";
 import { Language } from "./src/language.js";
+
+const compilerLanguage1 = {
+    lexicalRules: [
+        { name: 'IF', regex: /if/ },
+        { name: 'DECLARATION', regex: /let|var/ },
+        { name: 'WHILE', regex: /while/},
+        { name: 'THEN', regex: /then/},
+        { name: 'DO', regex: /do/},
+        { name: 'ID', regex: /[a-z][a-z0-9]*/ },
+        { name: 'NUM', regex: /[0-9]+/ },
+        { name: 'REAL', regex: /([0-9]+"."[0-9]*)|([0-9]*"."[0-9]+)/ },
+        { name: 'WHITESPACE', regex: /("--"[a-z]*"\n")|(" "|"\n"|"\t")+/ },
+        { name: 'ASSIGN', regex: /=/ },
+        { name: 'LPAR', regex: /\(/ },
+        { name: 'RPAR', regex: /\)/ },
+        { name: 'MULT', regex: /\*/ },
+        { name: 'SEMI', regex: /;/},
+        { name: 'PLUS', regex: /\+/},
+        { name: 'MINUS', regex: /\-/},
+        { name: 'GTHAN', regex: />/},
+        { name: 'LTHAN', regex: /</},
+        { name: 'LBRKT', regex: /{/},
+        { name: 'RBRKT', regex: /}/},
+    ],
+    exampleCode1: "let v =2; v = v + 2; v = v*v + 2*(v-1)",
+    exampleCode2: "while (v < 20) do { if(a > 3) v = k+2}"
+}
+
+const compilerLanguage2 = {
+    lexicalRules: [
+        { name: 'PRINT', regex: /print/ },
+        { name: 'ID', regex: /[a-z][a-z0-9]*/ },
+        { name: 'NUM', regex: /[0-9]+/ },
+        { name: 'ASSIGN', regex: /=/ },
+        { name: 'LPAR', regex: /\(/ },
+        { name: 'RPAR', regex: /\)/ },
+        { name: 'SEMI', regex: /;/},
+        { name: 'PLUS', regex: /\+/},
+        { name: 'MULT', regex: /\*/},
+        { name: 'DIV', regex: /\//},
+    ],
+    exampleCode1: "a*b*(b+c)/4",
+    exampleCode2: "while (v < 20) do { if(a > 3) v = k+2}"
+}
+
+const compilerLanguage3 = {
+    lexicalRules: [
+        { name: 'PRINT', regex: /print/ },
+        { name: 'BEGIN', regex: /begin/ },
+        { name: 'END', regex: /end/ },
+        { name: 'ID', regex: /[a-z][a-z0-9]*/ },
+        { name: 'NUM', regex: /[0-9]+/ },
+        { name: 'ASSIGN', regex: /=/ },
+        { name: 'LPAR', regex: /\(/ },
+        { name: 'RPAR', regex: /\)/ },
+        { name: 'SEMI', regex: /;/},
+        { name: 'PLUS', regex: /\+/},
+    ],
+    exampleCode1: "begin alecrim = dourado; dourado = dourado + alecrim + colorado; print(alecrim) end end end",
+}
 
 class Program{
 
-    constructor(){
+    constructor(compilerLanguage){
 
         this.Lexer  = new Lexer({
                         conflictResolution: "Rule Priority", 
-                        rules: [
-                            { name: 'IF', regex: /if/ },
-                            { name: 'DECLARATION', regex: /let|var/ },
-                            { name: 'WHILE', regex: /while/},
-                            { name: 'THEN', regex: /then/},
-                            { name: 'DO', regex: /do/},
-                            { name: 'ID', regex: /[a-z][a-z0-9]*/ },
-                            { name: 'NUM', regex: /[0-9]+/ },
-                            { name: 'REAL', regex: /([0-9]+"."[0-9]*)|([0-9]*"."[0-9]+)/ },
-                            { name: 'WHITESPACE', regex: /("--"[a-z]*"\n")|(" "|"\n"|"\t")+/ },
-                            { name: 'ASSIGN', regex: /=/ },
-                            { name: 'LPAR', regex: /\(/ },
-                            { name: 'RPAR', regex: /\)/ },
-                            { name: 'MULT', regex: /\*/ },
-                            { name: 'SEMI', regex: /;/},
-                            { name: 'PLUS', regex: /\+/},
-                            { name: 'MINUS', regex: /\-/},
-                            { name: 'GTHAN', regex: />/},
-                            { name: 'LTHAN', regex: /</},
-                            { name: 'LBRKT', regex: /{/},
-                            { name: 'RBRKT', regex: /}/},
-                        ]
+                        rules: compilerLanguage.lexicalRules
                     });
 
         const language = new Language(this.Lexer.tokenNames, [], []);
 
-        language.addProductionRule("E", ["E", "+", "E"]);
-        language.addProductionRule("E", ["ID"]);
-        language.addProductionRule("E", ["NUM"]);
-        language.addProductionRule("E", ["(", "S","," ,"E", ")"]);
-        language.addProductionRule("S", ["S", ";", "S"]);
-        language.addProductionRule("S", ["ID", ":", "=", "E"]);
-        language.addProductionRule("L", ["E"]);
-        language.addProductionRule("L", ["L", ",", "E"]);
+        language.addProductionRule("E", ["E", "+", "T"])
+        language.addProductionRule("E", ["E", "-", "T"])
+        language.addProductionRule("E", ["T"])
 
-        this.Parser = new Parser({
+        language.addProductionRule("T", ["T", "*", "F"])
+        language.addProductionRule("T", ["T", "/", "F"])
+        language.addProductionRule("T", ["F"])
+
+        language.addProductionRule("F", ["ID"])
+        language.addProductionRule("F", ["NUM"])
+        language.addProductionRule("F", ["(", "E", ")"])
+
+        this.Parser = new PredictiveParser({
                         language: language
-                    });     
+                    });    
+                    
+                    
+        console.log(language.productionRules)
     }
 
     run(sourceCode){
@@ -58,7 +103,4 @@ class Program{
     }
 }
 
-var code1 = "let v =2; v = v + 2; v = v*v + 2*(v-1)";
-var code2 = "while (v < 20) do { if(a > 3) v = k+2}"
-
-console.log(new Program().run(code2))
+console.log(new Program(compilerLanguage3).run(compilerLanguage3.exampleCode1))
--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,5 @@
 import { Language } from "./language.js";
+import util from 'util'
 
 export class Parser{
 
@@ -10,6 +11,112 @@ export class Parser{
     }
     
     parse(tokens){
+        this.tokens = tokens.map(t => t).reverse();
 
+        this.token = this.getToken();
+
+        this.S();
+
+    }
+
+    getToken(){
+        return this.tokens.pop();
+    }
+}
+
+export class PredictiveParser extends Parser{
+
+    constructor(settings={
+        language:null
+    }){
+        super(settings);
+
+        this.node = {};
+
+        this.syntaxTree = {Start: this.node};
+
+    }
+
+    advance(){
+
+        this.token = this.getToken();
+
+        console.log(util.inspect(this.syntaxTree, {depth: 10, colors: true}))
+    }
+
+    eat(token){
+
+        if(this.token.type == token) {
+            this.advance();
+            this.node[token] = token;
+        }
+
+        else throw Error("Token esperado: " + this.token.type + "; Token recebido " + token);
+    }
+
+    S(){
+        const node = this.node;
+
+        this.addNode("S");
+
+        switch(this.token.type){
+
+            case "ID": this.eat("ID"); this.eat("ASSIGN"); this.E(); this.L(); break;
+            case "BEGIN": this.eat("BEGIN"); this.S(); this.L(); break;
+            case "PRINT": this.eat("PRINT"); this.eat("LPAR"); this.E(); this.eat("RPAR"); break;
+            default: throw Error("Token não identificado " + this.token.type);
+        }
+
+        this.node = node;
+    }
+
+    L(){
+
+        const node = this.node;
+
+        this.addNode("L");
+
+        switch(this.token.type){
+
+            case "END": this.eat("END"); break;
+            case "SEMI": this.eat("SEMI"); this.S(); this.L(); break;
+            default: throw Error("Token não identificado" + this.token.type);
+        }
+
+        this.node = node;
+    }
+
+    E(){
+
+        const node = this.node;
+
+        this.addNode("E");
+
+        this.eat("ID");
+
+        while(true){
+
+            const tokens = [this.tokens[0], this.tokens[1]]
+
+            try{
+                this.eat("PLUS");
+                this.eat("ID");
+            }
+            catch(e){
+
+                this.tokens.concat(tokens)
+                break;
+            }
+
+        }
+
+        this.node = node;
+    }
+
+    addNode(type){
+
+        this.node[type] = {};
+
+        this.node = this.node[type];
     }
 }
