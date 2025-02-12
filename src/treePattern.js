5a8822a0d4fa5d520fdfa5e5df099455e10d9f09
GH120 <felipevieiraduarte195@gmail.com>
2025-02-12 06:21:20 -0300

    Adicionado mapaeamento de nome aos nós da árvore

--- src/treePattern.js
+++ src/treePattern.js
@@ -18,7 +18,7 @@ const structure1 = {
 }
 
 const test1 = {
-    type: "StatementList",
+    [`${'type'}`]: "StatementList", //Deixando aqui só para lembrar que é possível usar template literals
     children: [
         {
             type: "Statement", 
@@ -38,9 +38,67 @@ const test1 = {
     ]
 }
 
+const nodeNames = {
+    pai:[
+        {filhoEsquerda: [
+            'neto1',
+            'neto2',
+            'neto3'
+        ]},
+        {filhoMeio: [
+            'if',
+            'expressão',
+            'block'
+        ]},
+        'filhoDireita'
+    ]
+}
+
 class TreePattern {
 
-    process(structure){
+
+    constructor(structure, nodeNames){
+
+        this.matchPattern = this.createPatternChecker(structure);
+        this.matchNames   = this.createNameMatcher(nodeNames);
+    }
+
+    process(tree){
+        if(this.matchPattern(tree)){
+
+            const nameList = this.matchNames(tree);
+
+            //Objeto que mapeia cada nome a um nó da árvore que satisfez um padrão
+            const nameMap = nameList.reduce((obj, nameEntry) => {return {...obj, ...nameEntry}}, {});
+
+            return nameMap;
+        }
+
+        return false;
+    }
+
+    //Mapeia cada nó da estrutura a um nome designado
+    //Retorna lista de (nome: node)
+    createNameMatcher(nodeNames){
+
+        if(typeof nodeNames == 'string') return node => {return [{[`${nodeNames}`]: node}]};
+
+        for(const [nodeName, childrenNames] of Object.entries(nodeNames)){
+
+            //retorna a função que agrupa todos os mapeamentos 
+            return node => {
+                
+                const currentNodeEntry = this.createNameMatcher(nodeName)(node)
+                
+                const childrenNodeEntries = node.children.flatMap((child,i) => this.createNameMatcher(childrenNames[i])(child))
+
+                return currentNodeEntry.concat(childrenNodeEntries);
+            }
+        }
+    }
+
+    //Vê se os nós correspondem a uma estrutura de árvore baseada em seus tipos
+    createPatternChecker(structure){
 
         const isExpectedType = expectedType => node => node.type == expectedType || expectedType == "any";
 
@@ -49,7 +107,7 @@ class TreePattern {
         for(const [expectedNodeType, expectedChildren] of Object.entries(structure)){
 
             //lista de funções que verificam se cada filho satisfaz a estrutura
-            const isExpectedChild = expectedChildren.map(child => this.process(child));
+            const isExpectedChild = expectedChildren.map(child => this.createPatternChecker(child));
 
             //retorna a função que determina se a criança lida é a esperada
             return node => isExpectedType(expectedNodeType)(node) && node.children.every((child,i) => isExpectedChild[i](child));
@@ -57,4 +115,4 @@ class TreePattern {
     }
 }
 
-new TreePattern().process(structure1)(test1);
+new TreePattern(structure1, nodeNames).process(test1);
